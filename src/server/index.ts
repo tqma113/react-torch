ffd30684b6eb3f7fa345800a12b1b521fd07d4b7
tqma <tqma@ctrip.com>
2019-09-29 17:18:09 +0800

    fix

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import Koa from 'koa'
 import React from 'react'
 import ReactDomServer from 'react-dom/server'
+import viewEngine from 'koa-react-view-typescript'
 import { ServerRender, ViewProps } from '../index'
 import generateView from '../view'
 
@@ -17,20 +18,7 @@ export const serverRender: ServerRender = async (src, container) => {
 
   let finalContainer = container || ROOT
 
-  const viewProps: ViewProps = {
-    container: finalContainer,
-    content: contentStr,
-    initialState: {},
-    publicPath: '',
-    assets: {
-      index: 'string',
-      vendor: 'string',
-    }
-  }
-
-  app.use(ctx => {
-    ctx.body = generateView(viewProps)
-  })
+  app.use(viewEngine())
 
   return app
 }

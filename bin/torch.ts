1f0497b1f8c783882c35907ac98b9ac0de6441cd
Ma Tianqi <mtq1997@126.com>
2020-03-03 01:34:11 +0800

    fix

--- bin/torch.ts
+++ bin/torch.ts
@@ -4,31 +4,32 @@ import program from 'commander'
 import { dev, build, start } from '../index'
 
 program
-  .version('1.0.6')
+  .version('1.0.7')
+  .name('torch')
 
 program
   .command('dev')
   .description(`start development mode at ${process.cwd()}`)
-  .option('-d, --dir', 'root path')
-  .option('-p, --port', 'listening port')
-  .action((_, [dir, port]) => {
-    dev(dir,port)
+  .option('-d, --dir <dir>', 'root path')
+  .option('-p, --port <port>', 'listening port')
+  .action(({ dir, port }) => {
+    dev(dir, port)
   })
 
 program
   .command('build')
   .description(`build project at ${process.cwd()}`)
-  .option('-d, --dir', 'root path')
-  .action((_, [dir]) => {
+  .option('-d, --dir <dir>', 'root path')
+  .action(({ dir }) => {
     build(dir)
   })
 
 program
   .command('start')
   .description(`start production mode at ${process.cwd()}`)
-  .option('-d, --dir', 'root path')
-  .option('-p, --port', 'listening port')
-  .action((_, [dir, port]) => {
+  .option('-d, --dir <dir>', 'root path')
+  .option('-p, --port <port>', 'listening port')
+  .action(({ dir, port }) => {
     start(dir, port)
   })
 

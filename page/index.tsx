f7c6ebb577ca079cab5a95ea175eda0ad11d0d91
tqma <tqma@trip.com>
2020-05-19 19:46:54 +0800

    fix(page): createPage type

--- page/index.tsx
+++ page/index.tsx
@@ -9,7 +9,16 @@ export type Page<
   Store<S, AS>
 ]
 
-export function createPage<
+export type PageWithoutStore = [
+  React.ComponentType<{}>,
+  null
+]
+
+function createPage(
+  View: React.ComponentType<{}>,
+): PageWithoutStore
+
+function createPage<
   S extends object = {},
   AS extends Actions<S> = {}
 >(
@@ -18,9 +27,32 @@ export function createPage<
     actions: Currings<S, AS>
   }>,
   store: Store<S, AS>
-): Page<S, AS> {
-  return [
-    ({ store }) =>  <View state={store.state} actions={store.actions} />,
-    store
-  ]
+): Page<S, AS>
+
+function createPage<
+  S extends object = {},
+  AS extends Actions<S> = {}
+>(
+  View: React.ComponentType<{
+    state: S,
+    actions: Currings<S, AS>
+  }> | React.ComponentType<{}>,
+  store?: Store<S, AS>
+): Page<S, AS> | PageWithoutStore {
+  if (!store) {
+    return [
+      // @ts-ignore
+      () =>  <View />,
+      null
+    ] as PageWithoutStore
+  } else {
+    return [
+      ({ store }: { store: Store<S, AS> }) =>  <View state={store.state} actions={store.actions} />,
+      store
+    ]
+  }
+}
+
+export {
+  createPage
 }

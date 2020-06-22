e780603c2a406f2eba22457f0c17a179fff17bf2
tqma <tqma@trip.com>
2020-06-22 13:45:42 +0800

    rm mfs

--- src/dev/compile/index.ts
+++ src/dev/compile/index.ts
@@ -2,20 +2,18 @@ import webpack from 'webpack'
 import webpackDevMiddleware from 'webpack-dev-middleware'
 import getWebpackConfig from './webpackConfig'
 import reporter from './reporter'
-import { getMFS } from '../mfs'
 import type { IntegralTorchConfig } from '../../index'
 
 export default function compile(config: IntegralTorchConfig) {
   const webpackConfig = getWebpackConfig(config)
   const compiler = webpack(webpackConfig)
-  const mfs = getMFS()
   const middleware = webpackDevMiddleware(compiler, {
     publicPath: 'static',
     stats: {
       chunks: false,
       colors: true
     },
-    fs: mfs,
+    writeToDisk: true,
     serverSideRender: true,
     reporter
   })
--- src/dev/render/compile.ts
+++ src/dev/render/compile.ts
@@ -1,8 +1,8 @@
 import vm from 'vm'
+import fs from 'fs'
 import path from 'path'
 import webpack from 'webpack'
 import getConfig from './webpackConfig'
-import { getMFS } from '../mfs'
 import { matchExternals, getExternals } from './utils'
 import type { Router } from './router'
 import type { IntegralTorchConfig } from '../../index'
@@ -10,9 +10,7 @@ import type { IntegralTorchConfig } from '../../index'
 export default function compile(config: IntegralTorchConfig, router: Router) {
   const webpackConfig = getConfig(config)
   const compiler = webpack(webpackConfig)
-  const mfs = getMFS()
 
-  compiler.outputFileSystem = mfs
   compiler.watch({}, (err, stats) => {
     if (err) throw err
 
@@ -32,7 +30,7 @@ export default function compile(config: IntegralTorchConfig, router: Router) {
       let moduleResult = 'default'
 
       try {
-        sourceCode = mfs.readFileSync(filePath, 'utf-8')
+        sourceCode = fs.readFileSync(filePath, 'utf-8')
       } catch (_) {
         /**
          * externals 和 mfs 里没有这个文件
@@ -70,7 +68,7 @@ export default function compile(config: IntegralTorchConfig, router: Router) {
     }
 
     const outputPath = path.join(config.dir, '.torch', 'server', 'routes.js')
-    const sourceCode: string = mfs.readFileSync(outputPath, 'utf-8')
+    const sourceCode: string = fs.readFileSync(outputPath, 'utf-8')
     const newModule = runCode(sourceCode)
     if (newModule) {
       const routes = newModule.default || newModule

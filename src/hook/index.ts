18550c85a2d013ccc608f3a3e28212acb0617563
tqma <tqma@trip.com>
2020-07-02 14:07:45 +0800

    style

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -1,18 +1,6 @@
-import { setHook } from '../lifecircle'
-import type { ConfigHook, Hook } from '../lifecircle'
-
-export function useConfig(hook: ConfigHook) {
-  setHook('config', hook)
-}
-
-export function useWillCreate(hook: Hook) {
-  setHook('willCreate', hook)
-}
-
-export function useWillMount(hook: Hook) {
-  setHook('willMount', hook)
-}
-
-export function useDidMount(hook: Hook) {
-  setHook('didMount', hook)
-}
+export {
+  useConfig,
+  useWillCreate,
+  useDidMount,
+  useWillMount
+} from './lifecircle'

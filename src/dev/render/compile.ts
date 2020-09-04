cdeb7981113a6ef0c01413ceccaf21b67452edeb
tqma <tqma@trip.com>
2020-09-04 18:40:34 +0800

    fix(dev): require cache

--- src/dev/render/compile.ts
+++ src/dev/render/compile.ts
@@ -18,20 +18,31 @@ export default function compile(
   const compiler = webpack(webpackConfig)
 
   return new Promise<void>((resolve, reject) => {
+    const serverPath = path.join(
+      config.dir,
+      TORCH_DIR,
+      TORCH_SERVER_DIR,
+      TORCH_ROUTES_FILE_NAME
+    )
     compiler.watch({}, (err, stats) => {
       if (err) reject(err)
 
       const statsObj = stats.toJson()
       statsObj.errors.forEach(console.error)
       statsObj.warnings.forEach(console.warn)
+      statsObj.assets?.forEach((asset) => {
+        const assetPath = path.join(
+          serverPath,
+          asset.name
+        )
+        delete require.cache[require.resolve(assetPath)];
+      })
 
-      const outputPath = path.join(
-        config.dir,
-        TORCH_DIR,
-        TORCH_SERVER_DIR,
+      const routesPath = path.join(
+        serverPath,
         TORCH_ROUTES_FILE_NAME
       )
-      const newModule = require(outputPath)
+      const newModule = require(routesPath)
       if (newModule) {
         const routes = newModule.default || newModule
         update(routes)
